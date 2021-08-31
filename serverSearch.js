ddc0a278ab1f063c37f7aa51b18e0964b87cd379
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-31 12:06:42 +0200

    Jerry 2.0.423
    [*] Removed Wanaheda to Samora Machel Consi.... renaming.

--- serverSearch.js
+++ serverSearch.js
@@ -813,13 +813,6 @@ redisCluster.on("connect", function () {
                 //Get the location
                 new Promise((res) => {
                   let tmpTimestamp = search_timestamp;
-                  //Replace wanaheda by Samora Machel Constituency
-                  request.query = /(wanaheda|wanahe|wanahed)/i.test(
-                    request.query
-                  )
-                    ? "Samora Machel Constituency"
-                    : request.query;
-                  //...
                   getLocationList_five(
                     request.query,
                     request.city,
