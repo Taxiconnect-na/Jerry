7a06fa16b5407006f2399dbcce04200259b84a97
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-10-16 11:02:36 +0200

    Jerry 2.0.498
    [*] Decrease requests garbage collector time to 3 min.

--- serverWatcher.js
+++ serverWatcher.js
@@ -2415,7 +2415,27 @@ redisCluster.on("connect", function () {
             //     logger.info(error);
             //   });
 
-            //?2. Mark as offline all the inactive drivers
+            //? 2. Clean X hold requests
+            new Promise((res1) => {
+              removeOldRequests_madeWithoutBeingAttended(
+                collectionPassengers_profiles,
+                collectionRidesDeliveryData,
+                res1
+              );
+            })
+              .then(
+                (result) => {
+                  //logger.info(result);
+                },
+                (error) => {
+                  //logger.info(error);
+                }
+              )
+              .catch((error) => {
+                //logger.info(error);
+              });
+
+            //?3. Mark as offline all the inactive drivers
             new Promise((res2) => {
               autoOfflineInactiveDrivers(
                 collectionDrivers_profiles,
@@ -2467,26 +2487,6 @@ redisCluster.on("connect", function () {
 
           //! FOR LIGHT HEAVY PROCESSES REQUIRING - 5min
           cron.schedule("*/5 * * * *", function () {
-            //? 1. Clean X hold requests
-            new Promise((res1) => {
-              removeOldRequests_madeWithoutBeingAttended(
-                collectionPassengers_profiles,
-                collectionRidesDeliveryData,
-                res1
-              );
-            })
-              .then(
-                (result) => {
-                  //logger.info(result);
-                },
-                (error) => {
-                  //logger.info(error);
-                }
-              )
-              .catch((error) => {
-                //logger.info(error);
-              });
-
             //? 2. Keep the drivers next payment date UP TO DATE
             new Promise((res2) => {
               updateNext_paymentDateDrivers(
